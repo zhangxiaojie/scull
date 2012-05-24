717d225c4301c8e7416a5f2d9473088a2dd2c6f9
zhang <zhang@zhang-laptop.(none)>
2012-05-24 11:02:45 +0800

    3rd commit

--- mknod.py
+++ mknod.py
@@ -1,7 +1,7 @@
 import os
 file=open('/proc/devices')
 for line in file:
-    if('blan' in line):
+    if('scull' in line):
         major,devname=line.split()
 ret=os.popen('mknod /dev/'+devname+' c '+major+' 0')
 f=open('/dev/scull','rb')
